4d26e1d3afeba2cf3681d9c0c5c431bf7e85c5fa
Glitch (hello-jquery6) <none>
2020-10-16 15:22:05 +0000

    🎊⛸ Checkpoint ./main.js:23431284/460 ./index.html:23431284/748

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 
 $.getJSON( "https://api.coindesk.com/v1/bpi/currentprice.json", function( data ) {
   var items = [];
-  $.each( data["", function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  $.each( data["bpi"], function( key1, val1 ) {
+    $.each( val1, function( key2, val2 ) {
+      items.push( "<li id='" + key2 + "_" + key1 + "'>" + val2 + "</li>" );
+    });
     
+  $( "<ul/>", {
+    "id": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );    
   });
  
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+
 });
