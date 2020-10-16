b5d721b7fb8c630889f548adfa0239c9603bd34f
Glitch (hello-jquery6) <none>
2020-10-16 15:34:06 +0000

    🏊🏼🎈 Checkpoint ./main.js:23431284/1821 ./index.html:23431284/33

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-
-$.getJSON( "https://api.coindesk.com/v1/bpi/currentprice.json", function( data ) {
-  var items = [];
-  $.each( data["bpi"], function( key1, val1 ) {
-    $.each( val1, function( key2, val2 ) {
-      items.push( "<li id='" + key2 + "_" + key1 + "'>" + val2 + "</li>" );
+$.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", function(data) {
+  $.each(data["bpi"], function(key1, val1) {
+    var items = [];
+    $.each(val1, function(key2, val2) {
+      items.push(
+        "<li class='liste' id='" + key2 + "_" + key1 + "'>" + val2 + "</li>"
+      );
     });
-    
-  $( "<ul/>", {
-    "id": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );    
-  });
- 
 
+    $("<h1/>", {
+      class: "titre",
+      id: key1 + "_title",
+      html: key1
+    }).appendTo("body");
+
+    $("<ul/>", {
+      class: "corps",
+      id: key1,
+      html: items.join("")
+    }).appendTo("body");
+  });
+
+  var Date = data["time"]["updated"];
+
+  $("<h3/>", {
+    class: "Date",
+    id: "Date",
+    html: Date
+  }).appendTo("body");
 });
